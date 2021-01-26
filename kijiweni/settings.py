59160b92e622e5aa7238d517a8e874573b35487c
Athman Bakari <disguisedsandwich@gmail.com>
2021-01-26 17:54:59 +0300

    Added user registration and profile

--- kijiweni/settings.py
+++ kijiweni/settings.py
@@ -120,6 +120,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Email 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER=os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
